485224a9ee0eaa1a79cd29c9073a60a17b1023c1
Tanvir321110 <tanvirahmed321110@gmail.com>
2026-07-21 19:57:20 +0600

    slider added

--- static/js/home.js
+++ static/js/home.js
@@ -1,15 +1,39 @@
 
 // ================ For Offer Slider ================
 new Swiper('.offer-slider-items', {
-      slidesPerView: 1,
-      spaceBetween: 24,
-      navigation: {
-            nextEl: '.offer-slider-next',
-            prevEl: '.offer-slider-prev',
+   slidesPerView: 1,
+   spaceBetween: 24,
+   navigation: {
+      nextEl: '.offer-slider-next',
+      prevEl: '.offer-slider-prev',
+   },
+   breakpoints: {
+      768: {
+         slidesPerView: 2,
       },
-      breakpoints: {
-            768: {
-                  slidesPerView: 2,
-            },
-      },
+   },
+});
+
+
+new Swiper('.recent-search-slider', {
+   slidesPerView: 1.15,
+   spaceBetween: 12,
+   loop: true,
+   speed: 4000,
+   allowTouchMove: true,
+   grabCursor: true,
+   observer: true,
+   observeParents: true,
+   autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: false,
+      stopOnLastSlide: false,
+      waitForTransition: true,
+   },
+   breakpoints: {
+      576: { slidesPerView: 2, spaceBetween: 14 },
+      992: { slidesPerView: 3, spaceBetween: 16 },
+      1200: { slidesPerView: 4, spaceBetween: 16 },
+   },
 });
