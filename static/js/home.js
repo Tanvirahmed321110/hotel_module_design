b0382c1cf3fde0bf1214fce1e80bb3383beed7f7
Tanvir321110 <tanvirahmed321110@gmail.com>
2026-07-22 18:50:31 +0600

    update slider

--- static/js/home.js
+++ static/js/home.js
@@ -37,3 +37,39 @@ new Swiper('.recent-search-slider', {
       1200: { slidesPerView: 4, spaceBetween: 16 },
    },
 });
+
+
+// ================ Trending Destinations ================
+const trendingSliders = [];
+
+document.querySelectorAll('.trendign-destinations .trending-slider').forEach((slider) => {
+   trendingSliders.push(new Swiper(slider, {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      speed: 650,
+      grabCursor: true,
+      observer: true,
+      observeParents: true,
+      pagination: {
+         el: slider.querySelector('.trending-pagination'),
+         clickable: true,
+      },
+   }));
+});
+
+const trendingSection = document.querySelector('.trendign-destinations');
+
+if (trendingSection) {
+   const tabButtons = trendingSection.querySelectorAll('.h-tab-btn');
+   const tabContents = trendingSection.querySelectorAll('.tab-content');
+
+   tabButtons.forEach((button, index) => {
+      button.addEventListener('click', () => {
+         tabButtons.forEach((item) => item.classList.remove('active'));
+         tabContents.forEach((item) => item.classList.remove('active'));
+         button.classList.add('active');
+         tabContents[index]?.classList.add('active');
+         trendingSliders[index]?.update();
+      });
+   });
+}
